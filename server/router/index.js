a09a9a319ef417e11b76a450223820bd7b53f2b8
尹平开 <240701740@qq.com>
2020-10-14 13:45:11 +0800

    test

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,10 @@
 const Router = require('koa-router');
 const user = require('./user');
 const router = new Router({ prefix: '/api' })
+router.get(`/search`, async ctx => {
+    
+    ctx.body = 'hellow world'
+})
 
 let routerMap = {
     user
