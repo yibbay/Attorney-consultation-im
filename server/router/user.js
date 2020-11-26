fd99084613bb1117e40ed7bd7a5f4046520c553a
尹平开 <240701740@qq.com>
2020-11-26 17:42:56 +0800

    test

--- server/router/user.js
+++ server/router/user.js
@@ -138,7 +138,10 @@ module.exports = (router) => {
     router.post(`${PREFIX}/pdf`, async (ctx) => {
         let { pdfPageUrl, companyName, name } = ctx.request.body
         console.log(ctx.request.body)
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox',
+                    '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
         await page.goto(pdfPageUrl, { waitUntil: 'networkidle2' });
         await page.pdf({ path: `pdf_down/${companyName}-${name}.pdf`});
